38d0c49b8352f078a75fac126b2cf457d342d45b
Larisa Nazarenko <loranazarenko@users.noreply.github.com>
2021-09-08 21:39:40 +0300

    Add files via upload

---
+++ src/main/java/com/nayax/intern/microservices/document_manager/dto/CustomerDto.java
@@ -0,0 +1,53 @@
+package com.nayax.intern.microservices.document_manager.dto;
+
+public class CustomerDto {
+
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String address;
+
+    public CustomerDto() {
+    }
+
+
+    public CustomerDto(Long id, String firstName, String lastName, String address) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ src/main/java/com/nayax/intern/microservices/document_manager/dto/EventDto.java
@@ -0,0 +1,30 @@
+package com.nayax.intern.microservices.document_manager.dto;
+
+public class EventDto {
+    private long id;
+    private long documentId;
+
+
+    public EventDto(){};
+    public EventDto(long documentId, long id, String state) {
+        this.documentId = documentId;
+        this.id = id;
+
+    }
+
+    public long getDocumentId() {
+        return documentId;
+    }
+
+    public void setDocumentId(long documentId) {
+        this.documentId = documentId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
